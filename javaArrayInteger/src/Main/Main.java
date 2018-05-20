21352828d0f3676a1d0db85230bf14d4147b7f02
Danil Ochagov <36893636+ochagovdanil@users.noreply.github.com>
2018-05-20 10:29:57 +0300

    Add files via upload

---
+++ javaArrayInteger/src/Main/Main.java
@@ -0,0 +1,10 @@
+package Main;
+
+import ArrayInteger.ArrayInteger;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayInteger arrayInteger = new ArrayInteger();
+        // code...
+    }
+}
